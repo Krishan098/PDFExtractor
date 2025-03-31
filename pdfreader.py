5edc8e1afe589290082da70886a805f0258f8df9
krim798 <168003081+Krishan098@users.noreply.github.com>
2025-03-31 21:03:17 +0530

    updated the gemini model to gemini-flash-2.0

--- pdfreader.py
+++ pdfreader.py
@@ -4,7 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -12,7 +12,8 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-api_key = os.getenv("GOOGLE_API_KEY")
+#api_key = os.getenv("GOOGLE_API_KEY")
+api_key=st.secrets["api_credentials"]["GOOGLE_API_KEY"]
 genai.configure(api_key=api_key)
 
 
@@ -51,7 +52,7 @@ def get_conversational_chain():
     Answer:
     """
     
-    model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
+    model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.3)
     prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
     
